7e02ade388e1a72ee6789757cfe22cbfd13981a6
Dmm91 <moya91.dmm@gmail.com>
2024-11-29 20:14:00 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let numeroCarta = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let palos = ["♦", "♥", "♠", "♣"];
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function cartaRandom() {
+    let numeroRandom = Math.floor(Math.random() * numeroCarta.length);
+    let paloRandom = Math.floor(Math.random() * palos.length);
+
+    document.getElementById("valorCarta").innerHTML = numeroCarta[numeroRandom];
+
+    document.getElementById("paloCartaArriba").innerHTML = palos[paloRandom];
+    document.getElementById("paloCartaAbajo").innerHTML = palos[paloRandom];
+
+    if (palos[paloRandom] === "♦" || palos[paloRandom] === "♥") {
+      document.getElementById("paloCartaArriba").style.color = "red";
+      document.getElementById("paloCartaAbajo").style.color = "red";
+    } else {
+      document.getElementById("paloCartaAbajo").style.color = "black";
+      document.getElementById("paloCartaArriba").style.color = "black";
+    }
+  }
+
+  document.getElementById("botonCarta").addEventListener("click", cartaRandom);
+  cartaRandom();
 };
